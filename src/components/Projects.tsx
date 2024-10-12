14ac3ebd0b254adc99aef80467b0c22afd28649a
valentinsg <sanchezguevaravalentin@gmail.com>
2024-10-12 17:40:58 -0300

    projects nuevo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { Heading, Grid, useColorModeValue, Box } from '@chakra-ui/react'
 import ProjectsContainer from './ProjectsContainer.';
 import StakingImage from '../img/staking.jpg';
+import StakingGif from '../img/video-staking.gif';
 
 const Projects = () => {
-
-  const headingColor = useColorModeValue('black','whiteAlpha.900');
-
+  const headingColor = useColorModeValue('black', 'whiteAlpha.900');
   return (
     <Box
     >
@@ -14,10 +13,15 @@ const Projects = () => {
         Featured Projects
       </Heading>
       <Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }} gap={8}>
-        <ProjectsContainer bgImage={StakingImage} />
-        <ProjectsContainer />
-        <ProjectsContainer />
-        <ProjectsContainer />
+        <ProjectsContainer
+          bgImage={StakingImage}
+          gifUrl={StakingGif}
+          title="Staking Project"
+          description="A decentralized staking platform for cryptocurrency"
+          technologies={['react', 'nodejs', 'mongodb']}
+        />        <ProjectsContainer title={''} description={''} technologies={[]} />
+        <ProjectsContainer title={''} description={''} technologies={[]} />
+        <ProjectsContainer title={''} description={''} technologies={[]} />
       </Grid>
     </Box>
   )
--- src/components/ProjectsContainer..tsx
+++ src/components/ProjectsContainer..tsx
@@ -1,35 +1,148 @@
-import React from 'react'
-import { GridItem, Heading, Text, useColorModeValue } from '@chakra-ui/react'
+import React, { useState } from 'react';
+import { Box, Heading, Text, useColorModeValue, Flex, Icon } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { FaReact, FaNodeJs, FaPython, FaHtml5, FaCss3Alt, FaJsSquare, FaGitAlt } from 'react-icons/fa';
+import { SiTypescript, SiMongodb, SiPostgresql } from 'react-icons/si';
 
 interface ProjectsContainerProps {
   bgImage?: string;
+  gifUrl?: string;
+  title: string;
+  description: string;
+  technologies: string[];
+  url?: string;
 }
 
-const ProjectsContainer: React.FC<ProjectsContainerProps> = ({ bgImage }) => {
+const ProjectsContainer: React.FC<ProjectsContainerProps> = ({ 
+  bgImage, 
+  gifUrl, 
+  title, 
+  description, 
+  technologies,
+  url
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
   const bgColor = useColorModeValue('gray.100', 'gray.800');
-  const headingColor = useColorModeValue('pink.500', 'pink.300');
-  const textColor = useColorModeValue('gray.700', 'gray.300');
 
   return (
-    <GridItem
+    <Box
+      as={motion.div}
       w="100%"
-      h="375px"
+      h={{ base: "300px", md: "375px" }}
       bg={bgColor}
-      p={6}
       borderRadius="lg"
-      boxShadow="md"
-      backgroundImage={bgImage}
-      backgroundSize="cover"
-      backgroundPosition="center"
+      boxShadow="lg"
+      overflow="hidden"
+      position="relative"
+      onHoverStart={() => setIsHovered(true)}
+      onHoverEnd={() => setIsHovered(false)}
+      onClick={() => window.open(url, '_blank', 'noopener,noreferrer')}
     >
-      <Heading as="h3" size="md" fontFamily="'Orbitron', sans-serif" color={headingColor}>
-        Project
-      </Heading>
-      <Text fontSize="sm" color={textColor}>
-        Brief description of project. Click to learn more.
-      </Text>
-    </GridItem>
-  )
-}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        backgroundImage={`url(${bgImage})`}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        opacity={isHovered ? 0 : 1}
+        transition="opacity 0.3s ease-in-out"
+      />
+      <Box
+        as="img"
+        src={isHovered ? gifUrl : ''}
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        objectFit="cover"
+        opacity={isHovered ? 1 : 0}
+        transition="opacity 0.3s ease-in-out"
+      />
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        bg="rgba(0,0,0,0.3)"
+        opacity={1}
+        transition="opacity 0.3s ease-in-out"
+      />
+      <Flex
+        direction="column"
+        justify="space-between"
+        h="100%"
+        p={6}
+        position="relative"
+        zIndex={1}
+      >
+        <Box>
+          <Heading
+            as="h3"
+            size="lg"
+            fontFamily="'Orbitron', sans-serif"
+            color="white"
+            textShadow="2px 2px 4px rgba(0,0,0,0.6)"
+            mb={2}
+          >
+            {title}
+          </Heading>
+          <Text
+            fontSize="md"
+            color="white"
+            fontWeight="medium"
+            textShadow="1px 1px 2px rgba(0,0,0,0.6)"
+          >
+            {description}
+          </Text>
+        </Box>
+        <Flex wrap="wrap" mt={4}>
+          {technologies.map((tech, index) => (
+            <Icon
+              key={index}
+              as={getIconForTechnology(tech)}
+              boxSize={6}
+              mr={2}
+              mb={2}
+              color="white"
+              filter="drop-shadow(2px 2px 2px rgba(0,0,0,0.6))"
+            />
+          ))}
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
 
-export default ProjectsContainer
+const getIconForTechnology = (tech: string) => {
+  switch (tech.toLowerCase()) {
+    case 'react':
+      return FaReact;
+    case 'nodejs':
+      return FaNodeJs;
+    case 'python':
+      return FaPython;
+    case 'html':
+      return FaHtml5;
+    case 'css':
+      return FaCss3Alt;
+    case 'javascript':
+      return FaJsSquare;
+    case 'typescript':
+      return SiTypescript;
+    case 'mongodb':
+      return SiMongodb;
+    case 'postgresql':
+      return SiPostgresql;
+    case 'git':
+      return FaGitAlt;
+    default:
+      return FaReact;
+  }
+};
+
+export default ProjectsContainer;
