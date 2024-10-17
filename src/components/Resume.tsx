16aa428a814e5e9c8d08b3eaa3760064a21c98a2
valentinsg <sanchezguevaravalentin@gmail.com>
2024-10-17 00:32:34 -0300

    nuevos cambios finales en resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Heading, Text, List, ListItem, ListIcon, Link, Button, Image, Grid, Flex, Icon, Tooltip, Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton, Avatar } from '@chakra-ui/react';
+import { Box, Heading, Text, List, ListItem, ListIcon, Link, Button, Image, Grid, Flex, Tooltip, Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton, Avatar } from '@chakra-ui/react';
 import { FaCheckCircle, FaGithub, FaLinkedin, FaPhone } from 'react-icons/fa';
 import { SiMailgun } from 'react-icons/si';
 import Valentin from '../img/valentin.png';
@@ -7,7 +7,7 @@ import Valentin from '../img/valentin.png';
 const Resume = () => {
   // Colores y temas
   const accentColor = 'pink.300';
-  const textColor = 'gray.600';
+  const textColor = 'gray.500';
 
   // State for showing email and phone
   const [showEmail, setShowEmail] = useState(false);
@@ -19,36 +19,38 @@ const Resume = () => {
   return (
     <Box p={8}>
       {/* Encabezado */}
-      <Box textAlign="center" mb={10}>
-        <Box
-          border="2px solid pink"
-          borderRadius="full"
-          overflow="hidden"
-          width="150px"
-          height="150px"
-          mx="auto"
-          cursor="pointer"
-          onClick={() => setIsModalOpen(true)}
-        >
-          <Image src={Valentin} boxSize="150px" objectFit="cover" />
-        </Box>
-        <Heading as="h1" size="2xl" m={4} color={accentColor}>
+      <Box textAlign="center" mb={16}>
+        <Tooltip label="Show image" aria-label="Image Tooltip">
+          <Box
+            border="2px solid pink"
+            borderRadius="full"
+            overflow="hidden"
+            width="150px"
+            height="150px"
+            mx="auto"
+            cursor="pointer"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <Image src={Valentin} boxSize="150px" objectFit="cover" />
+          </Box>
+        </Tooltip>
+        <Heading as="h1" size="2xl" m={4} color={"whiteAlpha.900"}>
           Valentín Sánchez Guevara
         </Heading>
         <Text fontSize="xl" color={textColor} letterSpacing="tight">
           Developer | Creative | Entrepreneur
         </Text>
 
-        <Flex justifyContent="center" gap={8} m={6} >
-          <Tooltip label="Click to show email" aria-label="Email Tooltip">
+        <Flex justifyContent="center" gap={8} m={4} >
+          <Tooltip label="Show email" aria-label="Email Tooltip">
             <Flex alignItems="center" fontSize="lg" color={textColor} cursor="pointer" onClick={() => setShowEmail(!showEmail)}>
-              <SiMailgun size={"25px"} color='white'/>
+              <SiMailgun size={"25px"} color='white' />
               <Box ml={2} overflow="hidden" whiteSpace="nowrap" maxWidth={showEmail ? '300px' : '0'} transition="max-width 0.4s ease-out">
                 <Text>{showEmail ? 'sanchezguevaravalentin@gmail.com' : ''}</Text>
               </Box>
             </Flex>
           </Tooltip>
-          <Tooltip label="Click to show phone number" aria-label="Phone Tooltip">
+          <Tooltip label="Show phone number" aria-label="Phone Tooltip">
             <Flex alignItems="center" fontSize="lg" color={textColor} cursor="pointer" onClick={() => setShowPhone(!showPhone)}>
               <FaPhone size={"25px"} color='white' />
               <Box ml={2} overflow="hidden" whiteSpace="nowrap" maxWidth={showPhone ? '200px' : '0'} transition="max-width 0.4s ease-out">
@@ -70,23 +72,26 @@ const Resume = () => {
         </Flex>
       </Box>
 
-      <Grid templateColumns="repeat(2, 1fr)" gap={12}>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
         {/* Sección de Skills */}
         <Box mb={10}>
           <Heading color={accentColor} as="h2" fontSize="3xl" mb={6}>
             Skills
           </Heading>
           <Text fontSize="lg" color={textColor}>
-            <strong>Languages:</strong> React, TypeScript, Redux
+            <strong>Languages:</strong> JavaScript, TypeScript, Python.
           </Text>
           <Text fontSize="lg" color={textColor}>
-            <strong>UI Libraries:</strong> Chakra UI, Ionic
+            <strong>UI Libraries:</strong> Chakra UI, Ionic, Bootstrap.
           </Text>
           <Text fontSize="lg" color={textColor}>
-            <strong>Business Management Skills:</strong> Project Management, Client Relations
+            <strong>Frameworks / Libraries:</strong> React, Next.js, Redux.
           </Text>
           <Text fontSize="lg" color={textColor}>
-            <strong>English:</strong> B2
+            <strong>Marketing and Communication:</strong> Digital Marketing & Content Strategy, Community & Digital Engagement.
+          </Text>
+          <Text fontSize="lg" color={textColor}>
+            <strong>English:</strong> B2.
           </Text>
 
           {/* Sección de Educación */}
@@ -95,12 +100,29 @@ const Resume = () => {
               Education
             </Heading>
             <Text fontSize="lg" color={textColor}>
-              <strong>Bachelor's Degree in Computer Science</strong>
+              <strong>Bachelor's Degree in Social Sciences:</strong>
             </Text>
             <Text fontSize="lg" color={textColor}>
-              University of Technology, 2018 - 2022
+              IEPM - EESN°42.
+            </Text>
+            <Text fontSize="lg" color={textColor}>
+              <strong>Technical Degree in Software Development (UNFINISHED):</strong>
+            </Text>
+            <Text fontSize="lg" color={textColor}>
+              UTN MDP.
             </Text>
           </Box>
+          {/* Botón de descarga */}
+          <Tooltip label="Download Resume as PDF" aria-label="Download Tooltip">
+            <Button size={['md', 'lg']}
+              variant="outline"
+              boxShadow={'md'}
+              colorScheme="pink"
+              fontSize={{ base: "sm", md: "md", lg: "lg" }}
+              mt={8}>
+              Download Resume (PDF)
+            </Button>
+          </Tooltip>
         </Box>
 
         {/* Sección de Experiencia */}
@@ -116,15 +138,15 @@ const Resume = () => {
             <List spacing={3} mt={2} color={textColor}>
               <ListItem>
                 <ListIcon as={FaCheckCircle} color={accentColor} />
-                Dashboards and interactive graphs
+                Dashboards and interactive graphs.
               </ListItem>
               <ListItem>
                 <ListIcon as={FaCheckCircle} color={accentColor} />
-                Ticketing system implementation
+                Ticketing system implementation.
               </ListItem>
               <ListItem>
                 <ListIcon as={FaCheckCircle} color={accentColor} />
-                Chat integrations for support
+                Chat integrations for support.
               </ListItem>
             </List>
           </Box>
@@ -148,32 +170,27 @@ const Resume = () => {
               </ListItem>
               <ListItem>
                 <ListIcon as={FaCheckCircle} color={accentColor} />
-                Branding and presence on internet platforms for business
+                Branding and presence on internet platforms for business.
               </ListItem>
               <ListItem>
                 <ListIcon as={FaCheckCircle} color={accentColor} />
-                Digital marketing strategies.
+                Digital marketing.
               </ListItem>
             </List>
           </Box>
         </Box>
       </Grid>
 
-      {/* Botón de descarga */}
-      <Box textAlign="center" mt={8}>
-        <Tooltip label="Download Resume as PDF" aria-label="Download Tooltip">
-          <Button bg={accentColor} size="lg">
-            Download Resume (PDF) 
-          </Button>
-        </Tooltip>
-      </Box>
 
       {/* Modal for image */}
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} >
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <ModalOverlay />
-        <ModalContent w={"10vw"}>
+
+        <ModalContent w={"17vw"} bg="gray.500" borderRadius="100%" overflow="hidden">
           <ModalCloseButton />
+          <ModalBody p={0}>
             <Avatar size={"full"} src={Valentin} />
+          </ModalBody>
         </ModalContent>
       </Modal>
     </Box>
